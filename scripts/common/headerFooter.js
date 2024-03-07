c40b346c06b4fbdc34c823f76f3afa158214dcfa
simon <potelsmn@gmail.com>
2024-03-08 00:10:29 +0100

    error dir, TEMP fix of the bug

--- scripts/common/headerFooter.js
+++ scripts/common/headerFooter.js
@@ -6,7 +6,7 @@ function loadHeaderFooter() {
     const headerFooterContainer = document.getElementById('header-footer-container');
     const headerRequest = new XMLHttpRequest();
 
-    headerRequest.open('GET', '../../html/common/header.html');
+    headerRequest.open('GET', '../../smartswap-web/html/common/header.html');
     headerRequest.onload = function() {
         if (headerRequest.status === 200) {
             headerFooterContainer.innerHTML += headerRequest.responseText;
