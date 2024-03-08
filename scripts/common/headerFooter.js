1f6b8fde975ee30bb9d4e326780545bb9adf1a6b
simon <potelsmn@gmail.com>
2024-03-08 15:13:26 +0100

    avatar when logged + dashboard onclick, disconnect

--- scripts/common/headerFooter.js
+++ scripts/common/headerFooter.js
@@ -1,8 +1,3 @@
-// this file is used in all pages of the website to load the header from:
-// html/common/header.html and style/common.css
-
-// asynchronous load of the header to not get disturb with the others pages
-
 window.addEventListener('DOMContentLoaded', function() {
     loadHeaderFooter();
 });
@@ -15,7 +10,79 @@ function loadHeaderFooter() {
     headerRequest.onload = function() {
         if (headerRequest.status === 200) {
             headerFooterContainer.innerHTML += headerRequest.responseText;
+            handleLoggedInUser();
         }
     };
     headerRequest.send();
+}
+
+function handleLoggedInUser() {
+    const loggedInUser = sessionStorage.getItem('loggedInUser');
+    const userAvatar = document.getElementById('user-avatar');
+    const userName = document.getElementById('user-name');
+    const userInfoContainer = document.getElementById('user-info');
+    const disconnectClic = document.getElementById('disconnect-clic');
+    const loginButton = document.getElementById('login-button');
+    
+    if (loggedInUser) { 
+        loginButton.style.display = 'none';
+        
+        userInfoContainer.style.display = 'flex';
+        userInfoContainer.style.alignItems = 'center';
+        userInfoContainer.style.marginBottom = '40px';
+
+        userAvatar.src = `../../files/avatar.png`;
+        userAvatar.style.width = "30px";
+        userAvatar.style.height = "30px";
+        userAvatar.style.marginRight = '5px';
+
+        userName.textContent = loggedInUser; 
+        userName.style.color = 'white'; 
+        userName.style.marginLeft = '5px'; 
+        userName.style.marginRight = '5px';
+        userName.style.fontSize = '16px';
+        userName.style.marginTop = "-5px";
+
+        disconnectClic.style.marginTop = "3px";
+        disconnectClic.style.marginLeft = "25px";
+        disconnectClic.style.marginRight = "-50px";
+    } else {
+        userInfoContainer.style.display = 'none';
+        disconnectClic.style.display = 'none';
+
+        loginButton.style.display = 'block'; 
+    }
+
+
+    const logo = document.querySelector('.logo-main img');
+    const navLinks = document.querySelector('nav ul');
+
+    if (logo && navLinks) {
+        logo.addEventListener('click', function () {
+            window.location.href = '../../index.html';        
+        });
+
+        document.querySelector('.logo-main').addEventListener('click', function () {
+            if (window.innerWidth <= 768) {
+                if (navLinks.style.display === 'block') {
+                    navLinks.style.display = 'none';
+                } else {
+                    navLinks.style.display = 'block';
+                }
+            } else {
+                window.location.href = '../../index.html'; 
+            }
+        });
+    } else {
+        console.error("Logo or nav links not found in the document.");
+    }
+
+    if (disconnectClic) {
+        disconnectClic.addEventListener('click', function() {
+            sessionStorage.removeItem('loggedInUser'); 
+            location.reload(); 
+        });
+    } else {
+        console.error("Disconnect button not found in the document.");
+    }
 }
